b1d10622b3e7eabdf5ea4668c52a72965a309d04
Inna Soldatkina <inna.soldier@gmail.com>
2022-06-03 10:04:31 -0500

    added name card component

--- app.vue
+++ app.vue
@@ -60,10 +60,7 @@ import {Gender, Length, Popularity, names} from "@/data"
       </button>
     </div>
     <div class="cards-container">
-      <div v-for="name in selectedNames" :key="name" class="card">
-        {{name}}
-        <p>x</p>
-      </div>
+    <CardName v-for="name in selectedNames" :key="name" :name="name"/>
     </div>
   </div>
 </template>
@@ -105,26 +102,6 @@ import {Gender, Length, Popularity, names} from "@/data"
     display: flex;
     margin-top: 3rem;
     flex-wrap: wrap;
-  }
-
-  .card {
-    background-color: rgb(27,60,138);
-    width: 28%;
-    color: white;
-    border-radius: 1rem;
-    padding: 1rem;
-    margin-right: 0.5rem;
-    margin-bottom: 1rem;
-    position: relative;
-  }
-
-  .card p{
-    position: absolute;
-    top: -28%;
-    left: 92.5%;
-    cursor: pointer;
-    color: rgba(255,255,255,0.178);
-
   }
 
 </style>
