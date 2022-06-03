62afec2179b4b0d62c22e44209cce8f17bc88b0d
Inna Soldatkina <inna.soldier@gmail.com>
2022-06-03 09:50:21 -0500

    added option component

--- app.vue
+++ app.vue
@@ -25,6 +25,24 @@ import {Gender, Length, Popularity, names} from "@/data"
   };
 
   const selectedNames = ref<string[]>([])
+
+  const optionsArray = [
+    {
+      title: "1) Choose a gender",
+      category: "gender",
+      buttons: [Gender.GIRL, Gender.UNISEX, Gender.BOY],
+    },
+    {
+      title: "2) Choose the name's popularity",
+      category: "popularity",
+      buttons: [Popularity.TRENDY, Popularity.UNIQUE],
+    },
+    {
+      title: "3) Choose the name's length",
+      category: "length",
+      buttons: [Length.ALL, Length.LONG, Length.SHORT],
+    }
+  ]
 </script>
 
 <template>
@@ -32,60 +50,11 @@ import {Gender, Length, Popularity, names} from "@/data"
     <h1>Baby Name Generator</h1>
     <p>Choose your options and click the "Find Names" button below</p>
     <div class="options-container">
-      <div class="option-container">
-        <h4>1) Choose a gender</h4>
-        <div class="option-buttons">
-          <button
-              class="option option-left"
-              :class="options.gender === 'Boy' && 'option-active'"
-              @click="options.gender = Gender.BOY"
-          >Boy</button>
-          <button
-              class="option"
-              :class="options.gender === 'Unisex' && 'option-active'"
-              @click="options.gender = Gender.UNISEX"
-          >Unisex</button>
-          <button
-              class="option option-right"
-              :class="options.gender === 'Girl' && 'option-active'"
-              @click="options.gender = Gender.GIRL"
-          >Girl</button>
-        </div>
-      </div>
-      <div class="option-container">
-        <h4>1) Choose the name's popularity</h4>
-        <div class="option-buttons">
-          <button
-              class="option option-left"
-              :class="options.popularity === 'Trendy' && 'option-active'"
-              @click="options.popularity = Popularity.TRENDY"
-          >Trendy</button>
-          <button class="option option-right"
-                  :class="options.popularity === 'Unique' && 'option-active'"
-                  @click="options.popularity = Popularity.UNIQUE"
-          >Unique</button>
-        </div>
-      </div>
-      <div class="option-container">
-        <h4>1) Choose the name's length</h4>
-        <div class="option-buttons">
-          <button
-              class="option option-left"
-              :class="options.length === 'Long' && 'option-active'"
-              @click="options.length = Length.LONG"
-          >Long</button>
-          <button
-              class="option"
-              :class="options.length === 'All' && 'option-active'"
-              @click="options.length = Length.ALL"
-          >All</button>
-          <button
-              class="option option-right"
-              :class="options.length === 'Short' && 'option-active'"
-              @click="options.length = Length.SHORT"
-          >Short</button>
-       </div>
-      </div>
+      <Option v-for="option in optionsArray"
+              :key="option.title"
+              :option="option"
+              :options="options"
+      />
       <button class="primary" @click="computeSelectedNames">
         Find Names
       </button>
@@ -118,37 +87,7 @@ import {Gender, Length, Popularity, names} from "@/data"
     padding: 1rem;
     width: 95%;
     margin: 0 auto;
-    margin-top: 4rem;
     position: relative;
-  }
-
-  .option-container {
-    margin-bottom: 2rem;
-  }
-
-  .option {
-    background-color: white;
-    outline: 0.15rem solid rgb(249,87,89);
-    border: none;
-    padding: 0.75rem;
-    width: 12rem;
-    font-size: 1rem;
-    color: rgb(27,60,138);
-    cursor: pointer;
-    font-weight: 200;
-  }
-
-  .option-left {
-    border-radius: 1rem 0 0 1rem;
-  }
-
-  .option-right {
-    border-radius: 0 1rem 1rem 0;
-  }
-
-  .option-active {
-    background-color: rgb(249,87,89);
-    color: white;
   }
 
   .primary {
